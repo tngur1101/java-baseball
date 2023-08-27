a357d8379f382766386419dd54a87c8d390165bf
kimsuhyeok <tngur1101@naver.com>
2023-08-27 23:59:26 +0900

    feat: Baseballs
    Baseballs class 생성
    해당 클래스는 balls를 담는 리스트를 만들어주는 클래스

---
+++ src/main/java/baseball/Baseballs.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Baseballs {
+
+    public static final int BALLS_CONTAINER_SIZE = 3;
+    private final List<Integer> balls;
+
+    public Baseballs(List<Integer> balls){
+        this.balls = balls;
+    }
+
+    public Baseballs(String rawBallsInput){
+        balls = Stream.of(rawBallsInput.split("")).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+}
