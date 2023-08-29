0deb13d422c3cde53ab0c01768d5b7d890711fde
kimsuhyeok <tngur1101@naver.com>
2023-08-29 20:06:13 +0900

    feat: Main
    테스트를 진행하기 위한 main 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Scanner;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         // TODO: 프로그램 구현
+        final String scanner = Console.readLine();
+        Player player = new Player(scanner);
+        Game game = new Game(scanner);
+
+        do{
+            game.play();
+            checkReplayDecisionOfPlayer(player);
+        }while (player.getReplayDecision()==Player.WANT_REPLAY);
     }
+
+    private static void checkReplayDecisionOfPlayer(Player player){
+//        try {
+//            player.checkReplayDecision();
+//        }catch (IllegalArgumentException e){
+//            System.out.println(e.getMessage());
+//            checkReplayDecisionOfPlayer(player);
+//        }
+        player.checkReplayDecision();
+    }
+
 }
