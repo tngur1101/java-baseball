57b5dd4b8722ebcdb8f2ec2e3156f30cbf7c0c04
kimsuhyeok <tngur1101@naver.com>
2023-08-30 00:33:34 +0900

    fix: Application
    Game객체 생성 및 실행 외 다른 코드 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,26 +7,10 @@ import java.util.Scanner;
 public class Application {
     public static void main(String [] args) {
         // TODO: 프로그램 구현
-//        final String scanner = Console.readLine();
-//        Player player = new Player();
         Game game = new Game();
 
-//        do{
-//            game.play();
-//            checkReplayDecisionOfPlayer(player);
-//        }while (player.getReplayDecision()==Player.WANT_REPLAY);
         game.play();
 
-    }
-
-    private static void checkReplayDecisionOfPlayer(Player player){
-//        try {
-//            player.checkReplayDecision();
-//        }catch (IllegalArgumentException e){
-//            System.out.println(e.getMessage());
-//            checkReplayDecisionOfPlayer(player);
-//        }
-        player.checkReplayDecision();
     }
 
 }
